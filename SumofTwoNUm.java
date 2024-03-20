7e6ee8395554bae508bf5659ad9473167845b8fd
Atulsigh7898 <maniharatul@gmail.com>
2024-03-21 01:12:22 +0530

    first commit

---
+++ SumofTwoNUm.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class SumofTwoNUm {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Input the first number :");
+
+        int num1 = input.nextInt();
+
+        System.out.println("Input the second Number : ");
+
+        int num2 = input.nextInt();
+
+        int sum = num1 + num2;
+
+        System.out.println();
+        
+        System.out.println("sum: " + sum);
+
+    }
+}
